2eaea337d11de891ae175abebd2d0ceb4a236d7f
orkunsecer <orkunsecer@gmail.com>
2026-06-18 19:40:04 +0300

    Update Commit

--- 1_Grain_Boundary_Detection.py
+++ 1_Grain_Boundary_Detection.py
@@ -11,8 +11,8 @@ nx, ny   = 384, 128
 R        = 4 * np.pi / np.sqrt(3)
 frac_R   = 0.20
 
-PSI_FILE = "/home/debian/PycharmProjects/PFC_2D_3D/Plots_P1800_v1_alpha0.1_beta0.025_M0.6_1/psi_1000.npy"
-OUT_PATH = "local_maxima_with_avg_angles_floored_1000_1.csv"
+PSI_FILE = "/home/debian/PycharmProjects/PFC_2D_3D/Plots_P1800_v0.02_alpha0.1_beta0.025_M0.6_1/psi_15750.npy"
+OUT_PATH = "local_maxima_with_avg_angles_floored_15000_1.csv"
 
 
 def load_psi(path, ny, nx):
